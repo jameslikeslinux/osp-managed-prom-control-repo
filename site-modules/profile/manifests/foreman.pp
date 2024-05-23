843045b93c4d72e09b6a42352ef199fb06818c70
James Lee <james@james.tl>
2024-05-23 17:52:02 -0400

    foreman: Make Puppet keys readable by Foreman

--- site-modules/profile/manifests/foreman.pp
+++ site-modules/profile/manifests/foreman.pp
@@ -1,4 +1,21 @@
 class profile::foreman {
   include foreman::repo
   include foreman
+
+  group { 'puppet':
+    system => true,
+  }
+
+  user { 'puppet':
+    gid    => 'puppet',
+    system => true,
+  }
+
+  # Foreman uses Puppet keys by default; make them readable
+  file { '/etc/puppetlabs/puppet/ssl':
+    owner   => 'puppet',
+    group   => 'puppet',
+    recurse => true,
+    before  => Class['foreman'],
+  }
 }
