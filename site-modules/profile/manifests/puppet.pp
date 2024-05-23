c0e098e2b2aabbe0f0b006692acc53a86fa60c2f
James Lee <james@james.tl>
2024-05-23 17:23:08 -0400

    puppet: Avoid common modules conflict with r10k

--- site-modules/profile/manifests/puppet.pp
+++ site-modules/profile/manifests/puppet.pp
@@ -2,15 +2,22 @@ class profile::puppet (
   Boolean $server = false,
 ) {
   if $server {
+    $common_modules_path = [
+      '/etc/puppetlabs/code/modules',
+      '/opt/puppetlabs/puppet/modules',
+      '/usr/share/puppet/modules'
+    ]
+
     class { 'puppet':
-      agent_server_hostname => $trusted['certname'],
-      autosign              => true,
-      environment           => 'production',
-      server                => true,
-      server_external_nodes => '',
-      server_foreman        => false,
-      server_reports        => 'puppetdb',
-      server_storeconfigs   => true,
+      agent_server_hostname      => $trusted['certname'],
+      autosign                   => true,
+      environment                => 'production',
+      server                     => true,
+      server_common_modules_path => $common_modules_path, # avoid conflict with r10k
+      server_external_nodes      => '',
+      server_foreman             => false,
+      server_reports             => 'puppetdb',
+      server_storeconfigs        => true,
     }
 
     include puppetdb
